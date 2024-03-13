322796e7591bc6791460e6369004cb19f8c9fb2f
catherina119 <310147895@qq.com>
2024-03-13 16:10:34 -0400

    class activity

--- models/book.js
+++ models/book.js
@@ -1,16 +1,14 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 
-var BookSchema = new Schema(
-  {
-    title: {type: String, required: true},
-    author: {type: Schema.Types.ObjectId, ref: 'Book', required: true },
-    summary: {type: String, required: true},
-    ISBN: {type: String, required: true},
-    genre: [{type: Schema.Types.ObjectId, ref: 'Genre'}],
-  }
-);
+var BookSchema = new Schema({
+  title: { type: String, required: true },
+  author: { type: Schema.Types.ObjectId, ref: "Book", required: true },
+  summary: { type: String, required: true },
+  isbn: { type: String, required: true },
+  genre: [{ type: Schema.Types.ObjectId, ref: "Genre" }],
+});
 
 //Export model
-module.exports = mongoose.model('Book', BookSchema);
+module.exports = mongoose.model("Book", BookSchema);
